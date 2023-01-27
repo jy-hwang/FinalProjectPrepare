5b9d35b017d9a136ca653a39c9551da6534b010a
jy-hwang <stratojy@gmail.com>
2023-01-27 22:34:17 +0900

    2023.01.27(금) 예제파일 commit & push

---
+++ src/jyHwang/javaPratice1/Q1.java
@@ -0,0 +1,15 @@
+package jyHwang.javaPratice1;
+
+public class Q1 {
+/*
+ 1.1~10 까지 각 수를 출력하는 프로그램을 main Method 에 작성하시오
+  (단, 출력형식은 1 2 3...으로 하시오)
+ */
+	public static void main(String[] args) {
+
+		for(int i = 1; i<11; i++) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
---
+++ src/jyHwang/sql/20230127_emp.sql
@@ -0,0 +1,12 @@
+/*
+scott 계정으로 진행하시오
+1. emp 테이블을 조회할 때 특정인임을 알 수 없도록 이름을 앞 2자 , 뒤 1자를 제외한 나머지를 * 로 마스킹 처리하시오. 마스킹 처리한 컬럼만 출력하고 이름은 MASKING 으로 하시오.
+	ex > SMITH -> SM**H
+ */
+
+
+/* 
+아래 부터는 hr계정으로 진행하시오
+2. 부서 테이블의 부서코드와 부서명을 검색하라
+*/
+SELECT DEPARTMENT_ID ,DEPARTMENT_NAME  FROM DEPARTMENTS;
