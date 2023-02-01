8e8472751c7bc1141fa5deb5b89a8f2424c64ff7
abocadoa <mdm9508@naver.com>
2023-02-01 19:20:34 +0900

    문자바풀어봄

--- src/dmMoon/javaPractice1/Q4.java
+++ src/dmMoon/javaPractice1/Q4.java
@@ -2,14 +2,14 @@ package dmMoon.javaPractice1;
 
 public class Q4 {
 
-	public static int sumNtoM(int n, int m) {
-		int sum=n+m;
+	public static void sumNtoM(int n, int m) {
+		int sum=0;
 		for(int i=n; i<=m; i++) {
 			sum+=i;
 		}
-		System.out.printf("n부터 m까지의 합계:", n, m, sum);
-		return sum;
+		System.out.printf("n부터 m까지의 합계:"+sum, n, m);
 	}
 	public static void main(String[] args) {
+		sumNtoM(10, 50);
 	}
 }
